f970986412e03692668915d2e1df98995f99b7c3
JojoBr0 <92635650+JojoBr0@users.noreply.github.com>
2022-10-30 12:53:32 +0100

    Change personal settings from 'additional' to 'security'
    Signed-off-by: JojoBr0 <92635650+JojoBr0@users.noreply.github.com>

--- lib/Settings/Personal.php
+++ lib/Settings/Personal.php
@@ -67,11 +67,21 @@ class Personal implements ISettings {
 		], TemplateResponse::RENDER_AS_BLANK);
 	}
 
-	public function getSection(): string {
-		return 'additional';
+	/**
+	 * @return string the section ID, e.g. 'sharing'
+	 */
+	public function getSection() {
+		return 'security';
 	}
 
-	public function getPriority(): int {
-		return 10;
+	/**
+	 * @return int whether the form should be rather on the top or bottom of
+	 * the admin section. The forms are arranged in ascending order of the
+	 * priority values. It is required to return a value between 0 and 100.
+	 *
+	 * E.g.: 70
+	 */
+	public function getPriority() {
+		return 50;
 	}
 }
